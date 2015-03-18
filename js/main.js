35f803bc16a366bdbf75b970eb31897cc37a174e
Allison Logan <agcandreva@yahoo.com>
2015-03-17 20:44:27 -0400

    jQuery adds four styles to p element

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready( function () {
+    var paragraphs = $( "p" );
+    paragraphs.css( {
+        "color": "green", 
+        "background-color": "black",
+        "font-weight": "bold",
+        "line-height": "2em"
+        });
+});
